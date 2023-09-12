d453b6ae6239354e288b506c614b69394398748f
Kaykobad Ahammed Yiasin <kaykobadyiasin@gmail.com>
2023-09-13 02:45:59 +0600

    footer edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="bg-[#004282] text-white px-4 py-8 xl:mt-0 lg:mt-16 md:mt-16 mt-0 text-center">
+        <footer className="bg-[#004282] text-white px-4 py-8 xl:mt-0 lg:mt-16 md:mt-16 mt-32 text-center">
             <div className="container mx-auto">
                 <p>&copy; 2023 Your Company. All Rights Reserved.</p>
             </div>
